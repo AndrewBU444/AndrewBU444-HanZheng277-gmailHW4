0319b4c3be90227684198102992900206696384c
Andrew Cabral <andrewcabral@crc-dot1x-nat-10-239-140-84.bu.edu>
2024-11-07 14:36:17 -0500

    added test for testing runtime error for random_utils

--- meal_max/meal_max/tests/test_random_utils.py
+++ meal_max/meal_max/tests/test_random_utils.py
@@ -1,9 +1,16 @@
-#import pytest
+import pytest
+import requests
+from unittest.mock import patch
+from meal_max.utils.random_utils import get_random
+
 #def test_get_random_success():
 
 #def test_get_random_value_error():
 
-#def test_get_random_timeout():
+def test_get_random_timeout(): #Test that get_random raises RuntimeError on a timeout
+    with patch('requests.get', side_effect=requests.exceptions.Timeout):
+        with pytest.raises(RuntimeError, match="Request to random.org timed out"):
+            get_random()
 
 #def test_get_random_request_exception():
 
